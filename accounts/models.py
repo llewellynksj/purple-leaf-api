5c3609ea9feb5907ebb110286f3663bec95af1d5
Kate L <llewellyn.ksj@gmail.com>
2024-07-27 13:53:04 +0100

    Initial Commit

---
+++ accounts/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
+class Account(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+  name = models.CharField(max_length=100)
+  image = models.ImageField(
+      upload_to='images/',
+      default='../default_profile'
+  )
+
+  class Meta:
+      ordering = ['-created_at']
+
+  def __str__(self):
+      return self.user.username
+  
+def create_account(sender, instance, created, **kwargs):
+  if created:
+      Account.objects.create(user=instance)
+
+
+post_save.connect(create_account, sender=User)
