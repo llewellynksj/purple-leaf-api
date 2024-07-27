c82469c9c6b55dec499a2cd169eb4912f9bc4f50
Kate L <llewellyn.ksj@gmail.com>
2024-07-27 14:21:37 +0100

    Add AccountDetail view and hook up in urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,5 @@ from accounts import views
 
 urlpatterns = [
   path('accounts/', views.AccountList.as_view()),
+  path('accounts/<int:pk>/', views.AccountDetail.as_view()),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Account
 from .serializers import AccountSerializer
+from django.http import Http404
 
 
 class AccountList(APIView):
@@ -9,3 +10,18 @@ class AccountList(APIView):
     accounts = Account.objects.all()
     serializer = AccountSerializer(accounts, many=True)
     return Response(serializer.data)
+
+
+class AccountDetail(APIView):
+  def get_object(self, pk):
+    try:
+      account = Account.objects.get(pk=pk)
+      return account
+    except Account.DoesNotExist:
+      raise Http404
+  
+  def get(self, request, pk):
+    account = self.get_object(pk)
+    serializer = AccountSerializer(account)
+    return Response(serializer.data)
+  
