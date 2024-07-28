04f66a4a0ffc3d5026fabd051de19df676bd7f3e
Kate L <llewellyn.ksj@gmail.com>
2024-07-28 23:00:10 +0100

    Add image validation to leaf serializer

--- leaves/serializers.py
+++ leaves/serializers.py
@@ -8,6 +8,19 @@ class LeafSerializer(serializers.ModelSerializer):
   account_id = serializers.ReadOnlyField(source='owner.account.id')
   account_image = serializers.ReadOnlyField(source='owner.account.image.url')
 
+  def validate_image(self, value):
+    if value.size > 2 * 1024 * 1024:
+        raise serializers.ValidationError('Image too large - file should be less than 2MB')
+    if value.image.height > 4096:
+        raise serializers.ValidationError(
+            'Image too large - image height should be no larger than 4096px'
+        )
+    if value.image.width > 4096:
+        raise serializers.ValidationError(
+            'Image too large - image width should be no larger than 4096px'
+        )
+    return value
+
   def get_is_owner(self, obj):
     request = self.context['request']
     return request.user == obj.owner
@@ -15,5 +28,5 @@ class LeafSerializer(serializers.ModelSerializer):
   class Meta:
     model = Leaf
     fields = [
-      'id', 'owner', 'created_at', 'updated_at', 'loss_type', 'name', 'parent_name1', 'parent_name2', 'dob_due_date', 'weight', 'image', 'memory'
+      'id', 'owner', 'created_at', 'updated_at', 'loss_type', 'name', 'parent_name1', 'parent_name2', 'dob_due_date', 'weight', 'image', 'memory', 'is_owner', 'account_id', 'account_image'
     ]
