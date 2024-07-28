46df4f19a1d4c7115f94ef0521181fd5ecf54e39
Kate L <llewellyn.ksj@gmail.com>
2024-07-28 22:57:06 +0100

    Add leaf serializer

--- leaves/serializers.py
+++ leaves/serializers.py
@@ -5,4 +5,15 @@ from .models import Leaf
 class LeafSerializer(serializers.ModelSerializer):
   owner = serializers.ReadOnlyField(source='owner.username')
   is_owner = serializers.SerializerMethodField()
+  account_id = serializers.ReadOnlyField(source='owner.account.id')
+  account_image = serializers.ReadOnlyField(source='owner.account.image.url')
+
+  def get_is_owner(self, obj):
+    request = self.context['request']
+    return request.user == obj.owner
   
+  class Meta:
+    model = Leaf
+    fields = [
+      'id', 'owner', 'created_at', 'updated_at', 'loss_type', 'name', 'parent_name1', 'parent_name2', 'dob_due_date', 'weight', 'image', 'memory'
+    ]
