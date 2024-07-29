fb19a1cf009e1aed4e436e10e8e971e06ed634d5
Kate L <llewellyn.ksj@gmail.com>
2024-07-29 09:35:26 +0100

    Add tests for leaf detail view

--- leaves/tests.py
+++ leaves/tests.py
@@ -24,3 +24,36 @@ class LeafListViewTests(APITestCase):
   def test_user_not_logged_in_cant_create_leaf(self):
     response = self.client.post('/leaves/', {'memory': 'test memory'})
     self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class LeafDetailViewTests(APITestCase):
+  def setUp(self):
+    joe = User.objects.create_user(username='joe', password='bloggs')
+    jane = User.objects.create_user(username='jane', password='doe')
+    Leaf.objects.create(
+        user=joe, memory='joes test memory'
+    )
+    Leaf.objects.create(
+        user=jane, memory='janes test memory'
+    )
+
+  def test_can_retrieve_leaf_using_valid_id(self):
+    response = self.client.get('/leaves/1/')
+    self.assertEqual(response.data['memory'], 'joes test memory')
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+  def test_cant_retrieve_leaf_using_invalid_id(self):
+    response = self.client.get('/leaves/1234/')
+    self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+  def test_user_can_update_own_leaf(self):
+    self.client.login(username='joe', password='bloggs')
+    response = self.client.put('/leaves/1/', {'memory': 'a new memory'})
+    leaf = Leaf.objects.filter(pk=1).first()
+    self.assertEqual(leaf.memory, 'a new memory')
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+  def test_user_cant_update_another_users_leaf(self):
+    self.client.login(username='joe', password='bloggs')
+    response = self.client.put('/leaves/2/', {'memory': 'a new memory'})
+    self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
