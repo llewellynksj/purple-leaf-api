fce6fb9ebe21b33041916e4cacd379e459937b2f
Kate L <llewellyn.ksj@gmail.com>
2024-07-29 13:23:55 +0100

    Install corsheaders and update settings for deployment

--- pl_api/urls.py
+++ pl_api/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from .views import root_route
 
 urlpatterns = [
+    path('', root_route),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('dj_rest_auth/', include('dj_rest_auth.urls')),
