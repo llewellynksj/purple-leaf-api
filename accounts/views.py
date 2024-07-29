9f98164a07a91e594f729cf1a3e3342f771a8258
Kate L <llewellyn.ksj@gmail.com>
2024-07-29 09:59:11 +0100

    Refactor accounts and leaves views to use generic views

--- accounts/views.py
+++ accounts/views.py
@@ -1,41 +1,21 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics
 from .models import Account
 from .serializers import AccountSerializer
-from django.http import Http404
-from rest_framework import status
 from pl_api.permissions import IsOwnerOrReadOnly
 
 
-class AccountList(APIView):
-  def get(self, request):
-    accounts = Account.objects.all()
-    serializer = AccountSerializer(accounts, many=True, context={'request': request})
-    return Response(serializer.data)
-
-
-class AccountDetail(APIView):
+class AccountList(generics.ListAPIView):
+  """
+  List all accounts
+  """
+  queryset = Account.objects.all()
   serializer_class = AccountSerializer
-  permission_classes = [IsOwnerOrReadOnly]
 
-  def get_object(self, pk):
-    try:
-      account = Account.objects.get(pk=pk)
-      self.check_object_permissions(self.request, account)
-      return account
-    except Account.DoesNotExist:
-      raise Http404
-  
-  def get(self, request, pk):
-    account = self.get_object(pk)
-    serializer = AccountSerializer(account, context={'request': request})
-    return Response(serializer.data)
-  
-  def put(self, request, pk):
-    account = self.get_object(pk)
-    serializer = AccountSerializer(account, data=request.data, context={'request': request})
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-  
+
+class AccountDetail(generics.RetrieveUpdateAPIView):
+  """
+  Retrieve or update an account if you're the owner
+  """
+  permission_classes = [IsOwnerOrReadOnly]
+  queryset = Account.objects.all()
+  serializer_class = AccountSerializer
