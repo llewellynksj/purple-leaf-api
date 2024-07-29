463bd931b2f948090b03ef5b91ed5a428fee49c2
Kate L <llewellyn.ksj@gmail.com>
2024-07-29 13:38:18 +0100

    Add allowed host env variable

--- pl_api/settings.py
+++ pl_api/settings.py
@@ -61,7 +61,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['api-pl-e0a4e939123b.herokuapp.com', 'localhost', ]
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'), 'localhost',]
 
 
 # Application definition
