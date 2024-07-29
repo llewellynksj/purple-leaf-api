8e82753aa46cc44abd51187dfa40f666a7f20f17
Kate L <llewellyn.ksj@gmail.com>
2024-07-29 13:27:03 +0100

    Amend allowed host

--- pl_api/settings.py
+++ pl_api/settings.py
@@ -61,7 +61,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['https://api-pl-e0a4e939123b.herokuapp.com', 'localhost',]
+ALLOWED_HOSTS = ['api-pl-e0a4e939123b.herokuapp.com', 'localhost', ]
 
 
 # Application definition
